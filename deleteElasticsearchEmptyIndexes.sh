334cfa7bb1a92fcdc9073c11c9218d60f8482c2a
Julian Rubino <jrubino@elementum.com>
2021-06-03 12:36:44 -0300

    deleteElasticsearchEmptyIndexes

---
+++ deleteElasticsearchEmptyIndexes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+es_endpoint=""
+for index in $(curl -XGET "http://$es_endpoint/_all/_settings?pretty" -s | jq 'keys' | sed 's/["|,]//g' | grep -E '[[:alpha:]]+'); do
+    index_documents_number=$(curl -X GET "http://$es_endpoint/$index/_count" -s | jq '.count')
+    if (( $count == 0 )); then
+        echo "[+] Index $index is empty."
+        curl -XDELETE "http://$es_endpoint/$index"
+        echo "[+] Index $index deleted."
+    else·
+        echo "[+] Index $index documents quantity -> $index_documents_number"
+        echo "[+] Index $index will not be deleted"
+    fi
+done
